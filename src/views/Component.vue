4c8035da83ab047edeb102b279d006b8f81c5c26
MizunoKazuki <mizukazu.work@gmail.com>
2021-02-23 20:48:39 +0900

    Update transitionのテスト

--- src/views/Component.vue
+++ src/views/Component.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="component">
     <h1>カスタムコンポーネントをテストするページ</h1>
-    <div>
+    <div class="component_groups">
       <h2>カード</h2>
-      <m-card/>
+      <button @click="show=!show">切り替え</button>
+      <transition appear>
+         <m-card v-show="show" />
+      </transition>
     </div>
   </div>
 </template>
@@ -14,12 +17,33 @@ export default {
   name: 'component',
   components: {
     MCard
+  },
+  data() {
+    return {
+      show: true
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-h1 {
-  font-size: 1.5rem;
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s;
+}
+.v-enter,
+.v-leave {
+  opacity: 0;
+}
+.v-move {
+  transition: transform 1s;
+}
+
+.component {
+  display: flex;
+  flex-direction: column;
+  h1 {
+    font-size: 1.5rem;
+  }
 }
 </style>
